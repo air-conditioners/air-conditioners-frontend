d329269e2f752732816c648bb9750e0705c56913
유승하 <116069258+ysh2k06@users.noreply.github.com>
2023-07-13 03:31:55 +0900

    Final Commit
    마지막 커밋

--- trouble-conseling/src/styles/Header.style.js
+++ trouble-conseling/src/styles/Header.style.js
@@ -3,21 +3,6 @@ import styled from "styled-components";
 export const Image = styled.img`
     width: 50px;
     height: 50px;
-<<<<<<< Updated upstream
-    margin-top: 33px;
-    margin-left: 10px;
-`
-export const Header = styled.header`
-    height: 110px;
-    display: flex;
-    border-bottom: 2.5px solid #0078FF;
-`
-export const Title = styled.h2`
-    margin-top: 37px;
-    margin-left: 100px;
-    font-size: 27px;
-`
-=======
 `
 export const Header = styled.header`
     height: 110px;
@@ -35,4 +20,3 @@ export const HeaderSubWrap = styled.div`
     align-items: center;
     margin-left: 8rem;
 `;
->>>>>>> Stashed changes
