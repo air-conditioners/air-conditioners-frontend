3721d25053228381cdf1efb1cb376d41f93ad8a4
유승하 <116069258+ysh2k06@users.noreply.github.com>
2023-07-13 03:30:01 +0900

    Final Commit
    마지막 커밋

---
+++ trouble-conseling/src/styles/Footer.style.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+    width: 200px;
+    height: 100px;
+    position: fixed;
+    border-top: 2.5px solid #0078FF;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+`
+export const Content = styled.div`
+    width: 200px;
+    height: 50px;
+    margin-left: 40px;
+    margin-top: 25px;
+    background-color: #A5C4FF;
+    border-radius: 25px;
+    display: flex;
+    justify-content: space-between;
+`
+export const Msg = styled.img`
+    width: 30px;
+    height: 30px;
+    margin-top: 12px;
+    margin-left: 20px;
+    background-color: #A5C4FF;
+`
+export const Text = styled.p`
+    width: 130px;
+    height: 27px;
+    color: black;
+    font-size: 13px;
+`
--- trouble-conseling/src/styles/Header.style.js
+++ trouble-conseling/src/styles/Header.style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Image = styled.img`
     width: 50px;
     height: 50px;
+<<<<<<< Updated upstream
     margin-top: 33px;
     margin-left: 10px;
 `
@@ -16,3 +17,22 @@ export const Title = styled.h2`
     margin-left: 100px;
     font-size: 27px;
 `
+=======
+`
+export const Header = styled.header`
+    height: 110px;
+    border-bottom: 2.5px solid #0078FF;
+    display: flex;
+    align-items: center;
+`
+export const Title = styled.p`
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-right: 1rem;
+`
+export const HeaderSubWrap = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 8rem;
+`;
+>>>>>>> Stashed changes
